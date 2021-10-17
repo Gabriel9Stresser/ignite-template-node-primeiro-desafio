c0aaeb37193d233cb9277456e4ab22539378ec58
Gabriel9Stresser <gabriel.augusto99@hotmail.com>
2021-10-17 13:14:51 -0300

    (checksExistsUserAccount, /users)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { uuid } = request.headers;
+
+  const user = users.find((user) => user.uuid === uuid);
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { username, name } = request.body;
+
+  const userAlreadyExists = users.some(
+    (user) => user.cpf === cpf
+  );
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    username,
+    name,
+    id: uuidv4(),
+    todos: [],
+  });
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
